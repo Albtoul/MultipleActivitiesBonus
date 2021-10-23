31e391ab83ae7c16469f58608faf2c7efbf96eb9
albtoul <albtoul-kaseem@hotmail.com>
2021-10-23 22:22:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/multipleactivitiesbonus/RV.kt
@@ -0,0 +1,32 @@
+package com.example.multipleactivitiesbonus
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.multipleactivitiesbonus.databinding.ItemRowBinding
+
+
+class RV(private val entries: ArrayList<String>): RecyclerView.Adapter<RV.ItemViewHolder>() {
+    class ItemViewHolder(val binding: ItemRowBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val entry = entries[position]
+
+        holder.binding.apply {
+            tvItem.text = entry
+
+        }
+    }
+
+    override fun getItemCount() = entries.size
+
+    fun update(){
+        notifyDataSetChanged()
+    }
+}
